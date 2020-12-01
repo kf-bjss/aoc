9287b308b0e3b63ca404cf2718e0dbab963a83a2
Komol <komoliddin.fazliddin@bjss.com>
2020-12-01 10:03:26 -0500

    day 1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,28 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.toUpperCase() }
-            .joinToString(" ")
+        val inp = inputList.map { it.toInt() }
+        for (num1 in inp) {
+            for (num2 in inp) {
+                if ((num1 + num2) == 2020) {
+                    return (num1 * num2)
+                }
+            }
+        }
+        return -1
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.toUpperCase() }
-            .last()
+        val inp = inputList.map { it.toInt() }
+        for (num1 in inp) {
+            for (num2 in inp) {
+                for (num3 in inp) {
+                    if ((num1 + num2 + num3) == 2020) {
+                        return (num1 * num2 * num3)
+                    }
+                }
+            }
+        }
+        return -1
     }
 }
